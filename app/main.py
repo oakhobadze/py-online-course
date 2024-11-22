33616241722162ef4876900a0501cbc55a37c43f
Oleksandr Akhobadze <sandoahobadze@gmail.com>
2024-11-22 13:29:18 +0100

    adding solution to py-online-course

--- app/main.py
+++ app/main.py
@@ -1,3 +1,17 @@
+from __future__ import annotations
+
+
 class OnlineCourse:
-    # write your code here
-    pass
+    def __init__(self, name: str, description: str, weeks: int) -> None:
+        self.name = name
+        self.description = description
+        self.weeks = weeks
+
+    @staticmethod
+    def days_to_weeks(num: int) -> int:
+        return (num + 6) // 7
+
+    @classmethod
+    def from_dict(cls, course: dict) -> OnlineCourse:
+        days = cls.days_to_weeks(course["days"])
+        return cls(course["name"], course["description"], days)
